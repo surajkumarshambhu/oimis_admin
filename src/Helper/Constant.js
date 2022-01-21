794a81e953d3eff3bd953438180bd7c2e740f1d9
suraj.kumar.shambhu@gmail.com <suraj.kumar.shambhu@gmail.com>
2022-01-21 10:15:44 +0530

    url update

--- src/Helper/Constant.js
+++ src/Helper/Constant.js
@@ -1,6 +1,6 @@
 // export const BASEURL = 'http://127.0.0.1:8000/api/';
-export const BASEURL = 'http://admin.cnwworkshop.in/api/';
-export const URL = 'http://admin.cnwworkshop.in/';
+export const BASEURL = 'http://api.cnwworkshop.in/api/';
+export const URL = 'http://api.cnwworkshop.in/';
 
 export const ADMINURL = 'admin-login';
 export const GETPRODUCT = 'get-products';
